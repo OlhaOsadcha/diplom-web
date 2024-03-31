188df3ee1fbd6a115b0c8720379be8f93c852a21
olhaosadcha <olha.osadcha@gmail.com>
2024-03-31 13:57:03 +0100

    feat: Add eslint

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "tabWidth": 2,
+  "useTabs": false,
+  "singleQuote": true,
+  "semi": true,
+  "bracketSpacing": true,
+  "arrowParens": "avoid",
+  "trailingComma": "es5",
+  "bracketSameLine": true,
+  "printWidth": 100,
+  "htmlWhitespaceSensitivity": "strict"
+}
+
