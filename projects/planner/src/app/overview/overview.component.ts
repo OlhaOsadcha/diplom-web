f8b0d94e414462a3a1d0a36436d7c90c485806ac
olhaosadcha <olha.osadcha@gmail.com>
2024-04-13 19:10:02 +0100

    feat: Add spinner

--- projects/planner/src/app/overview/overview.component.ts
+++ projects/planner/src/app/overview/overview.component.ts
@@ -4,14 +4,17 @@ import { ChipConfig } from 'components';
 import { LIB_COLOR } from 'components';
 import { ShellService } from 'components';
 import { CardHeaderComponent } from '../shared/components/card-header/card-header.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrl: 'overview.component.scss',
   standalone: true,
-  imports: [CardComponent, CardHeaderComponent, ButtonComponent],
+  imports: [MatProgressSpinnerModule, CardComponent, CardHeaderComponent, ButtonComponent],
 })
 export class OverviewComponent implements OnInit {
+  public isLoading: boolean | undefined;
+
   constructor(private shellService: ShellService) {}
 
   public ngOnInit(): void {
@@ -23,6 +26,10 @@ export class OverviewComponent implements OnInit {
       displayString: 'Baseline',
       color: LIB_COLOR.accentGreen,
     };
+  }
+
+  public onViewIncome(): void {
+    this.isLoading = true;
   }
 
   private setHeaderConfig(): void {
