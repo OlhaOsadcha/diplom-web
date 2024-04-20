f61452f4b076a5e72034e4621828b273205c70d7
olhaosadcha <olha.osadcha@gmail.com>
2024-04-20 16:47:52 +0100

    feat: Extend income-detail

--- projects/planner/src/app/income/components/income-detail/income-detail.component.ts
+++ projects/planner/src/app/income/components/income-detail/income-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { ButtonComponent } from 'components';
 import { InputWrapperComponent } from '../../../../../../components/src/input/input-wrapper/input-wrapper.component';
 import { TextFieldDirective } from '../../../../../../components/src/input/text-field/text-field.directive';
 import { CheckboxComponent } from '../../../../../../components/src/input/checkbox/checkbox.component';
@@ -8,6 +10,39 @@ import { CheckboxComponent } from '../../../../../../components/src/input/checkb
   templateUrl: './income-detail.component.html',
   styleUrl: 'income-detail.component.scss',
   standalone: true,
-  imports: [CheckboxComponent, InputWrapperComponent, TextFieldDirective],
+  imports: [
+    CheckboxComponent,
+    InputWrapperComponent,
+    TextFieldDirective,
+    ButtonComponent,
+    ReactiveFormsModule,
+  ],
 })
-export class IncomeDetailComponent {}
+export class IncomeDetailComponent implements OnInit {
+  public incomeDetailForm!: UntypedFormGroup;
+  constructor(private fb: UntypedFormBuilder) {}
+
+  public ngOnInit(): void {
+    this.initForm();
+  }
+
+  public get showSpouse(): boolean {
+    return this.incomeDetailForm.get('hasSpouse')?.value;
+  }
+
+  private initForm(): void {
+    this.incomeDetailForm = this.fb.group({
+      salary: [''],
+      pension: [''],
+      arf: [''],
+      deposit: [''],
+      other: [''],
+      hasSpouse: [false],
+      salarySpouse: [''],
+      pensionSpouse: [''],
+      arfSpouse: [''],
+      depositSpouse: [''],
+      otherSpouse: [''],
+    });
+  }
+}
